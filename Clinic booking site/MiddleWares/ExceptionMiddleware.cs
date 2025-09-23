ffcee22fb0280487d13bcbc20dd290b87c2e8866
mohamed hesham <mohamedelhanafy290@gmail.com>
2025-09-24 00:49:01 +0300

    UpdatExceptionMiddleware

--- Clinic booking site/MiddleWares/ExceptionMiddleware.cs
+++ Clinic booking site/MiddleWares/ExceptionMiddleware.cs
@@ -23,6 +23,18 @@ namespace Clinic_booking_site.MiddleWares
             {
                 await _next(context);
             }
+            catch (ArgumentException ex)
+            {
+                _logger.LogWarning(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                var response = _env.IsDevelopment()
+                    ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    : new ApiException(context.Response.StatusCode, ex.Message);
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var json = JsonSerializer.Serialize(response, options);
+                await context.Response.WriteAsync(json);
+            }
             catch (Exception ex)
             {
                 _logger.LogError(ex, ex.Message);
