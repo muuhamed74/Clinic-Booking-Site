f6ef5642f65276633ff2b2e26d11833d7e8537e6
mohamed hesham <mohamedelhanafy290@gmail.com>
2025-11-25 20:27:34 +0200

    modifi sms provider token

--- Clinic.Service/Notifications Providers/NewSmsProvider .cs
+++ Clinic.Service/Notifications Providers/NewSmsProvider .cs
@@ -28,7 +28,7 @@ namespace Clinic.Service.Notifications_Providers
         public async Task SendAsync(string toPhoneNumber, int templateId, List<string>? variables = null)
         {
             using var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("gsor-token", _token);
+            client.DefaultRequestHeaders.Add("beon-token", _token);
 
             var payload = new Dictionary<string, object?>
             {
